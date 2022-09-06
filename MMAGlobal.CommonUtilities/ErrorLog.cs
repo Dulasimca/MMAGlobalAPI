d513c8927e94e9ae62ea4c15d18089bbf958db64
Dulasimca <dulasimca@gmail.com>
2022-09-06 14:33:28 +0530

    changes added

---
+++ MMAGlobal.CommonUtilities/ErrorLog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MMAGlobal.CommonUtilities
+{
+    public class ErrorLog
+    {
+        public static void WriteError(string sMessage, bool sendMail = false, EmailParameter emailParameter = null)
+        {
+            string fPath = string.Empty, sFileName = string.Empty;
+            try
+            {
+                fPath = AppDomain.CurrentDomain.BaseDirectory + "//LogFiles";
+                if (!Directory.Exists(fPath))
+                {
+                    Directory.CreateDirectory(fPath);
+                }
+                sFileName = DateTime.Now.ToString("ddMMyyyy");
+                StreamWriter sw = new StreamWriter(fPath + "//" + sFileName + ".txt", true);
+                sw.WriteLine(DateTime.Now + " : " + sMessage);
+                sw.Flush();
+                sw.Close();
+                //send mail to corresponding godown.
+                if (sendMail)
+                {
+                    MailSendings mailSendings = new MailSendings();
+                    mailSendings.SendMail(emailParameter);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                fPath = string.Empty; sFileName = string.Empty;
+            }
+        }
+    }
+}
---
+++ MMAGlobal.CommonUtilities/MailSendings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace MMAGlobal.CommonUtilities
+{
+    public class MailSendings
+    {
+        public void SendMail(EmailParameter emailParameter)
+        {
+            try
+            {
+                MailAddress to = new MailAddress(emailParameter.ToEmailID);
+                MailAddress from = new MailAddress(emailParameter.FromEmailID);
+                MailMessage mail = new MailMessage(from, to);
+                if (emailParameter != null)
+                {
+                    mail.CC.Add(new MailAddress(emailParameter.CCMailid));
+                }
+                mail.Subject = emailParameter.Subject;
+                mail.Body = emailParameter.BodyMessage;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = emailParameter.Host;
+                smtp.Port = emailParameter.Port;
+                smtp.Credentials = new NetworkCredential(emailParameter.FromEmailID, emailParameter.FromPassword);
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.WriteError(ex.Message);
+            }
+           
+        }
+    }
+    public class EmailParameter
+    {
+        public string FromEmailID { get; set; }
+        public string FromPassword { get; set; }
+        public string ToEmailID { get; set; }
+        public string CCMailid { get; set; }
+        public string Subject { get; set; }
+        public string BodyMessage { get; set; }
+        public int Port { get; set; }        
+        public string Host { get; set; }
+    }
+}
